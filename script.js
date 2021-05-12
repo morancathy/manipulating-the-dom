16150f4e3c9c3e7488d141c62b158c5650005f7e
Cat <morancathy@gmail.com>
2021-05-12 13:44:02 -0500

    Updates to Manipulating-the-dom

--- script.js
+++ script.js
@@ -12,37 +12,94 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const randomQuote = function() {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
-  randomQuote();
-  
-  // Do all of your work inside the document.addEventListener  
+  // randomQuote();
 
+  // Do all of your work inside the document.addEventListener
   // Part 1
-
+  const wordyTitle = () => {
+  const titleEl = document.getElementById('main-title');
+  titleEl.innerHTML = 'I am DOM. I have muscles.';
+  };
+  wordyTitle();
 
   // Part 2
-
+  const bodyChange = () => {
+    let bodyEl = document.querySelector('body');
+    bodyEl.style.backgroundColor = 'ghostwhite';
+  }
+  bodyChange();
 
   // Part 3
+  const favThings = () => {
+    let favThingsEl = document.querySelector("#favorite-things");
+    favThingsEl.removeChild(favThingsEl.lastElementChild);
+  }
+  favThings();
 
-
-  // Part 4
-
+  // // Part 4
+  const specialTitle = () => {
+    const item = document.querySelectorAll('.special-title');
+    item.forEach(function(bananas){
+    bananas.style.fontSize = '2rem';
+  })}
+  specialTitle();
 
   // Part 5
-
+  const pastRaces = () =>{
+    let item = document.querySelector('#past-races');
+    item.removeChild(item.children[3]);
+  };
+  pastRaces();
+// favorite = document.querySelector('#favorite-things > li:nth-child(6)');
 
   // Part 6
-
+  const newCity = () => {
+    const li = document.createElement('li');
+    li.innerHTML = 'Sydney';
+    document.getElementById('past-races').appendChild(li);
+  };
+  newCity();
 
   // Part 7
+  const blog = () => {
+    const div = document.createElement('div');
+    div.classList.add('.blog-post');
+    document.querySelector('#past-races').appendChild(div);
 
+    const newH2 = document.createElement('h2');
+    newH2.innerHTML = 'Sydney Blog Heading Text';
+    document.querySelector('#past-races div').appendChild(newH2);
+
+    const newP = document.createElement('p');
+    newP.innerHTML = 'I crashed into the Sydney Opera House';
+    document.querySelector("#past-races div").appendChild(newP);
+  };
+  blog();
 
   // Part 8
+  const quoteWork = () => {
+    const quoteEl = document.querySelector("#quote-title");
+    quoteEl.addEventListener("click", randomQuote);
+  };
+  quoteWork();
 
+  // Part 9cd
+  const blogWork = () => {    //this does not work
+    const blogEl = document.querySelectorAll(".blog-post");
+    console.log(blogEl)
 
-  // Part 9
-
-
-
+    blogEl.forEach(function(bananas){
+      bananas.addEventListener("mouseout", mouseOut);
+      function mouseOut(){
+        blogEl.classList.toggle('purple');
+      }
+      bananas.addEventListener("mouseenter", mouseEnter);
+      function mouseEnter(){
+        blogEl.classList.toggle('red');
+      }
+    }
+  )
+  }
+  blogWork();
 
 });
